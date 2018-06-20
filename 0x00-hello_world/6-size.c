1d6aae138a35d337bd926941a3dc09aa0c66ae0f
Heindrick Cheung <404@holbertonschool.com>
2018-06-20 23:50:54 +0000

    fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
