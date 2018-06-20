89517deff6df8d50140fc40310ec9bd6d640de73
Heindrick Cheung <404@holbertonschool.com>
2018-06-20 22:45:28 +0000

    push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n";
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -3,3 +3,5 @@ C-low level programming
 2. gcc -c *.c to change .c extension to .o
 3. gcc -S *.c to output .s files
 4. gcc $CFILE -o cisfun to output executabe file
+5. 4-puts.c file to print string using puts()
+6.
