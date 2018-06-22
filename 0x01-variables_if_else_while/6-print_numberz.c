b9ca7b93ce679f5b3c1bc0233cba3a0e0dd6d5e5
Heindrick Cheung <404@holbertonschool.com>
2018-06-22 00:24:27 +0000

    fixed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
