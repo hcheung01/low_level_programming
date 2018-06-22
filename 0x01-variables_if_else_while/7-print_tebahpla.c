686546c8b35a99a128943217a06dd523f43dd38e
Heindrick Cheung <404@holbertonschool.com>
2018-06-22 00:28:08 +0000

    fixed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--);
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
