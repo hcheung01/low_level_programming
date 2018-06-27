f72be1c2c56e2ae505d1a454f803f24b9ffde119
Heindrick Cheung <404@holbertonschool.com>
2018-06-27 03:16:28 +0000

    fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,35 @@
 #include "holberton.h"
 
 /**
+ * times_table - function without parameter
  *
- *
- *
- *
+ * Description: using for loops to create table and multiples
+ * Return: result
  */
 void times_table(void)
 {
-	
+	int i, j, result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
+		{
+			result = i * j;
+			if (result < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			} else if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
