a1fd1e06df46fa845023b54670e9e216ddd5c776
Heindrick Cheung <404@holbertonschool.com>
2018-06-27 01:15:12 +0000

    finished

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,15 +2,14 @@
 #include "stdio.h"
 
 /**
+ * print_to_98 - function with one argument
+ * @n: first operand
  *
- *
- *
- *
+ * Description: Print all natural numbers from n to 98
  */
 void print_to_98(int n)
 {
-
-	if (n > 98 )
+	if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
