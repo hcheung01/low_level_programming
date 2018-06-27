5c95dbbf283ef595a46445ec22cd968e1381130c
Heindrick Cheung <404@holbertonschool.com>
2018-06-27 02:33:37 +0000

    fixed typo

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,24 +8,18 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int hr, min;
 
-	for (i = '0'; i <= '2'; i++)
+	for (hr = '0'; hr <= '23'; hr++)
 	{
-		for (j = '0'; j <= '3'; j++)
+		for (min = '0'; min <= '59'; min++)
 		{
-			for (k = '0'; k <= '5'; k++)
-			{
-				for (l = '0'; l <= '9'; l++)
-				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(k);
-					_putchar(l);
-					_putchar('\n');
-				}
-			}
+			_putchar(hr / 10);
+			_putchar(hr % 10);
+			_putchar(':');
+			_putchar(min / 10);
+			-putchar(min % 10);
+			_putchar('\n');
 		}
 	}
 }
