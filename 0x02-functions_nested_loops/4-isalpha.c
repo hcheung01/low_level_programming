b9c928c641b8a6710e80d3425a75997927fab988
Heindrick Cheung <404@holbertonschool.com>
2018-06-27 00:35:36 +0000

    fixed betty

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 
-/*
- * _isalpha() to output return 1 if alphabet or return 0 for other
- * int c is the input
+/**
+ * _isalpha - to output return 1 if alphabet or return 0 for other
+ * @c: is the input
  *
- */ 
+ * Description: if/else to check for upper or lower case
+ * Return: return 1 if a letter, lower or uppercase and 0 is not
+ */
 int _isalpha(int c)
 {
 	if (c >= 'A' && c <= 'z')
