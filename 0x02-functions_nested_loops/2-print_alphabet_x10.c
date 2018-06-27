3dfd6071feebbdb52e5d872765be9d1091be4d9c
Heindrick Cheung <404@holbertonschool.com>
2018-06-27 01:41:15 +0000

    print 10 sets of alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - function with no arguments
+ *
+ * Description: prints 10 times the alphabet
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,6 +2,8 @@ int _putchar(char c);
 
 void print_alphabet(void);
 
+void print_alphabet_x10(void);
+
 int _islower(int c);
 
 int _isalpha(int c);
