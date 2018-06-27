e02241cb75dce05d8f3bab1ae24c5e1e2a87ba27
Heindrick Cheung <404@holbertonschool.com>
2018-06-27 01:11:59 +0000

    fixed

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,12 @@
 #include "holberton.h"
 
 /**
+ * add - to add two integers and return the result
+ * @num1: for operand number one
+ * @num2: for operand number two
  *
- * add() to add two integers and return the result
- * num1 for operand number one
- * num2 for operand number two
- * result integer variable is sum of num1 and num2
- * return for result
- *
+ * Description: result integer variable is sum of num1 and num2
+ * Return: result
  */
 int add(int num1, int num2)
 {
