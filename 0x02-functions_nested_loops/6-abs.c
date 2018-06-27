1526c559a72e58ceafec6f0bf0bcb0128743a755
Heindrick Cheung <404@holbertonschool.com>
2018-06-27 00:47:38 +0000

    fixed betty format

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 
+/**
+ * _abs - with one argument
+ * @a: First operand
+ *
+ * Description: using if/else statement to
+ * compute absolute value
+ * Return: absolute value
+ */
 int _abs(int a)
 {
 	if (a < 0)
 	{
-		return(a * -1);
+		return (a * -1);
 	}
 	else
-		return a;
-
+	{
+		return (a);
+	}
 }
