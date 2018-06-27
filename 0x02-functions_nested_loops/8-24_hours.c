db659465893b12787e5e756262e05216a270d5e9
Heindrick Cheung <404@holbertonschool.com>
2018-06-27 02:41:35 +0000

    fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,15 @@ void jack_bauer(void)
 {
 	int hr, min;
 
-	for (hr = '0'; hr <= '23'; hr++)
+	for (hr = 0; hr <= 23; hr++)
 	{
-		for (min = '0'; min <= '59'; min++)
+		for (min = 0; min <= 59; min++)
 		{
-			_putchar(hr / 10);
-			_putchar(hr % 10);
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
 			_putchar(':');
-			_putchar(min / 10);
-			-putchar(min % 10);
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
 			_putchar('\n');
 		}
 	}
