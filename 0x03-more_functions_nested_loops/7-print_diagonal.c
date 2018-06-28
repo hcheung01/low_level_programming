1a1ea834eea59bf090218831e5d042abbb479e05
Heindrick Cheung <404@holbertonschool.com>
2018-06-28 22:22:39 +0000

    fixed

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - function with one integer argument
+ * @n: 1st int type argument
+ *
+ * Description: draw a diagonal line on terminal with loops
+ * Return: na
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	i = 0;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			j = 0;
+			while (j <= i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	} else
+		_putchar('\n');
+}
