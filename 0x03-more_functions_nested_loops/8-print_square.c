2a08458da62e5e4e74c6343a77bda98506c20a10
Heindrick Cheung <404@holbertonschool.com>
2018-06-28 23:07:00 +0000

    nested for loops to print square

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_square - function with int type one argument
+ * @size: 1st int argument
+ *
+ * Description: nested loops to pring square with new line
+ * Return: na
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	} else
+	{
+		_putchar('\n');
+	}
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -16,3 +16,4 @@ void print_line(int n);
 
 void print_diagonal(int n);
 
+void print_square(int size);
