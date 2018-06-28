2907332ede3084a4e38ddd118025692feb1ee85a
Heindrick Cheung <404@holbertonschool.com>
2018-06-28 21:17:01 +0000

    fixed newline

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_line - function with one int type argument
+ * @n: first argument
+ *
+ * Description: while loop to draw a straight line
+ * Return: na
+ */
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	} else
+		_putchar('\n');
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -11,3 +11,5 @@ void print_numbers(void);
 void print_most_numbers(void);
 
 void more_numbers(void);
+
+void print_line(int n);
