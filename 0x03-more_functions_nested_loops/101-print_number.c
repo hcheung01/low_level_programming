febf916c9c2727095e399294c8711e87a90ebf74
Heindrick Cheung <404@holbertonschool.com>
2018-06-29 20:51:59 +0000

    fixed

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_number - function with one type int argument
+ * @n: 1st int type argument
+ *
+ * Description: if/else and recursion to print integer with putchar only
+ * Return: na
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
