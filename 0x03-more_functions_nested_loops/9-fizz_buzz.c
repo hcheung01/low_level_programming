2daeb1fcdb10ac050aebceededc9cb584912e601
Heindrick Cheung <404@holbertonschool.com>
2018-06-29 00:17:44 +0000

    fixed space

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 != 0 && i % 5 != 0)
 		{
@@ -20,6 +20,9 @@ int main(void)
 		} else if (i % 3 == 0)
 		{
 			printf("Fizz ");
+		} else if (i > 99)
+		{
+			printf("Buzz");
 		} else if (i % 5 == 0)
 		{
 			printf("Buzz ");
