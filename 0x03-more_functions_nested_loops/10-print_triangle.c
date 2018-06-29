062766e7ed7e3b9632aa8c2946e5d15a9d78507b
Heindrick Cheung <404@holbertonschool.com>
2018-06-29 02:36:57 +0000

    fixed

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - function with int type argument
+ * @size: int type 1st argument
+ *
+ * Description: nested loops to print triangle
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = j - i; k <= size; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -17,3 +17,5 @@ void print_line(int n);
 void print_diagonal(int n);
 
 void print_square(int size);
+
+void print_triangle(int size);
