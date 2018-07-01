61c3a28474827892e50330be3c2da9df4e49d990
Heindrick Cheung <404@holbertonschool.com>
2018-07-01 01:27:26 +0000

    fixed

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - function with int type single argument
+ * @n: int type single argument
+ *
+ * Description: if/else and nested loop to print multiple table
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	if (n < 15 && n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				result = i * j;
+				if (result > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				} else if (result > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				} else if (result < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -21,3 +21,5 @@ void times_table(void);
 int add(int, int);
 
 void print_to_98(int n);
+
+void print_times_table(int n);
