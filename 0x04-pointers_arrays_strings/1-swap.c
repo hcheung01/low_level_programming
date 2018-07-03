95f117f8e8334cb22c5ae14590065dedee516ab8
Heindrick Cheung <404@holbertonschool.com>
2018-07-03 20:14:49 +0000

    pointers to swap values of two variables

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * swap_int - function with two int type pointer argument
+ * @a: 1st argument int type
+ * @b: 2nd argument int type
+ *
+ * Description: Using pointer to change value of a and b
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
