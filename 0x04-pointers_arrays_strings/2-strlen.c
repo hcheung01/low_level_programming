c7afe15c2cc9e549d2b25110a4ea06eff2cd3f4f
Heindrick Cheung <404@holbertonschool.com>
2018-07-03 21:44:44 +0000

    update

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - function with 1 argument
+ * @s: char type pointer
+ *
+ * Description: strlen similiar function to return length
+ * Return: string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
