9df12f0d3d7a63e55591130a2e98a3e115e34abe
Heindrick Cheung <404@holbertonschool.com>
2018-07-04 22:12:05 +0000

    function to print half of a string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * puts_half - function with one argument
+ * @str: char type
+ *
+ * Description: prints half of a string
+ * Return: na
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+	if (len % 2 != 0)
+		len = (len + 1) / 2;
+	else
+		len /= 2;
+	while (len <= i)
+	{
+		len++;
+		_putchar(*(str + len - 1));
+	}
+	_putchar('\n');
+}
