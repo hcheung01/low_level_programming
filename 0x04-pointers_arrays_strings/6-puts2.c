5b87a3c74ed34e2c1d10ffa619eb13457b0c859c
Heindrick Cheung <404@holbertonschool.com>
2018-07-04 02:28:56 +0000

    fixed

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -13,11 +13,11 @@ void puts2(char *str)
 
 	while (*(str + i) != '\0')
 	{
-		if (*(str + i) != '\0')
+		if (*(str + i) % 2 == 0)
 		{
-		_putchar(*(str + i));
-		i+=2;
+			_putchar(*(str + i));
 		}
+		i++;
 	}
 	_putchar('\n');
 }
