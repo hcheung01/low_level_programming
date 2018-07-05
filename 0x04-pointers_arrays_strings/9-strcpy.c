a15f9a326169658e28c42792bd19df91fc31ce34
Heindrick Cheung <404@holbertonschool.com>
2018-07-05 00:36:34 +0000

    string copy with using strcpy function

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - function with two arguments
+ * @dest: 1st argument char type pointer
+ * @src: 2nd argument char type pointer
+ *
+ * Description: copies the string to an array
+ * Return: na
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
