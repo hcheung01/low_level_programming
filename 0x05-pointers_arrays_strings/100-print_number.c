8e3a88077513ddf2cce7b5d0d2059f7264ba74cf
Heindrick Cheung <hcheung01@gmail.com>
2018-07-08 23:25:11 +0000

    function to print an integer with putchar only

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -12,11 +12,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n = -n;
-		putchar('-');
+		_putchar('-');
 	}
 	if (n / 10)
 	{
 		print_number(n / 10);
 	}
-	putchar(n % 10 + '0');
+	_putchar(n % 10 + '0');
 }
