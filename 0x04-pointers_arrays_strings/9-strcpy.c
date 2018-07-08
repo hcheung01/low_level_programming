ece19729ef22c58fa7c495be668e479961e57ef6
Heindrick Cheung <hcheung01@gmail.com>
2018-07-08 21:53:17 +0000

    fixed

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
+	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
 	for ( ; i < *src; i++)
 		dest[i] = '\0';
 	return (dest);
