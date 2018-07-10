b698dd856ecc6ddb01d447681f34472e080c106f
Heindrick Cheung <hcheung01@gmail.com>
2018-07-10 00:28:10 +0000

    fixed

--- 0x05-pointers_arrays_strings/101-magic.c
+++ 0x05-pointers_arrays_strings/101-magic.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+	int n;
+	int a[5];
+	int *p;
 
-a[2] = 1024;
-p = &n;
+	a[2] = 1024;
+	p = &n;
 /*
  * write your line of code here...
  * Remember:
@@ -16,8 +16,8 @@ p = &n;
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
-*(p + 5);
+	*(p + 5);
 /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
