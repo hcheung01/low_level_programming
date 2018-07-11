85b53f2b3cf2a8655e378f700bf77906f740ff45
Heindrick Cheung <hcheung01@gmail.com>
2018-07-11 20:18:16 +0000

    fixed

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -10,10 +10,7 @@
 int _strlen_recursion(char *s)
 {
 	if (!*s)
-		return 0;
-	else
-	{
-		s++;
-		return 1 + _strlen_recursion(s);
-	}
+		return (0);
+	s++;
+	return ((1) + _strlen_recursion(s));
 }
