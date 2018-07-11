c9ab7b3d6a99257a299206e46754bcb025a436dd
Heindrick Cheung <hcheung01@gmail.com>
2018-07-11 01:43:37 +0000

    fixed

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -12,9 +12,9 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		for (j = 0; accept[j] = '\0'; j++)
 		{
 			if (accept[j] == s[i])
 				break;
