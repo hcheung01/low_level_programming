0513b22845935b1473285283598b4d98cd7ab842
Heindrick Cheung <hcheung01@gmail.com>
2018-07-12 01:50:19 +0000

    fixed description

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,22 +1,30 @@
 #include "holberton.h"
 
 /**
+ * counter - function with two argument
+ * @root: int type 1st argument
+ * @n: int type 2nd argument
+ *
+ * Description: to test conditions and return root
+ * Return: value of root
+ */
+int counter(int root, int n)
+{
+	if (n == (root * root))
+		return (root);
+	else if (root == n || root == -n)
+		return (-1);
+	else
+		return (counter(root + 1, n));
+}
+
+/**
  * _sqrt_recursion - function with one argument
  * @n: int type argument
  *
  * Description: returns the natural square root of a number
  * Return: value of n
  */
-int counter(int root, int n)
-{
-        if (n == (root * root))
-                return (root);
-        else if (root == n || root == -n)
-		return (-1);
-	else
-		return (counter(root + 1, n));
-}
-
 int _sqrt_recursion(int n)
 {
 	return (counter(0, n));
