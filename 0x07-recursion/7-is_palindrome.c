474383971bbf37a05ede434855d671322506a3f1
Heindrick Cheung <hcheung01@gmail.com>
2018-07-12 05:23:44 +0000

    updated description

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -15,6 +15,13 @@ int is_palindrome(char *s)
 	return (helper(s, findLength(s) - 1, 0));
 }
 
+/**
+ * findLength - function with 1 argument
+ * @s: char type pointer argument
+ *
+ * Description: To find length of string
+ * Return: total index count of string
+ */
 int findLength(char *s)
 {
 	if (*s != '\0')
@@ -25,6 +32,15 @@ int findLength(char *s)
 		return (0);
 }
 
+/**
+ * helper - helper function with 3 argument
+ * @s: char type pointer
+ * @len: int type argument
+ * @a: int type argument
+ *
+ * Description: to test conditions with all inputs from other functions
+ * Return: 1 if palindrome 0 if false
+ */
 int helper(char *s, int len, int a)
 {
 	if (len ==  a)
