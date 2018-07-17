d1f68b0721ebb249ded2d8462a862a0c97d367a6
Heindrick Cheung <hcheung01@gmail.com>
2018-07-17 22:48:09 +0000

    updated

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -14,16 +14,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr;
 
-	if (size == 0)
-		return (NULL);
-
 	ptr = malloc(sizeof(char) * size);
-        for (i = 0; i < size; i++)
+
+	if (size == 0 || ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
-	if (!ptr)
-		return (NULL);
-	else
-		return (ptr);
+	return (ptr);
 }
