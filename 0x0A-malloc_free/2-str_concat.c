70faa3bf5cc7bb6450753abec4c57fe4ceffc5ff
Heindrick Cheung <hcheung01@gmail.com>
2018-07-18 01:13:29 +0000

    updated

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -12,9 +12,12 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	char *empty = "";
 	int i, i2, j, k;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	i = 0;
 	while (s1[i] != '\0')
 		i++;
@@ -24,27 +27,20 @@ char *str_concat(char *s1, char *s2)
 
 	ptr = malloc(sizeof(char) * i + i2 + 1);
 
-	if (s1 != NULL)
+	j = 0;
+	while (s1[j] != '\0')
 	{
-		j = 0;
-		while (s1[j] != '\0')
-		{
-			ptr[j] = s1[j];
-			j++;
-		}
-	} else
-		ptr = empty;
-
-	if (s2 != NULL)
-	{
-		k = 0;
-		while (s2[k] != '\0')
-		{
-			ptr[j] = s2[k];
-			j++;
-			k++;
-		}
-		ptr[j] = '\0';
+		ptr[j] = s1[j];
+		j++;
 	}
+
+	k = 0;
+	while (s2[k] != '\0')
+	{
+		ptr[j] = s2[k];
+		j++;
+		k++;
+	}
+	ptr[j] = '\0';
 	return (ptr);
 }
