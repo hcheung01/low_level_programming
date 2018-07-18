3fd77b4be1aa1a0991081b68eb9e1f55d4f80f2f
Heindrick Cheung <hcheung01@gmail.com>
2018-07-18 20:46:26 +0000

    updated

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,5 +1,5 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - function with two argument
@@ -11,23 +11,31 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
 	int quarters, dimes, nickels, cent2, penny;
 	int remaining, remaining2, remaining3, final;
 
-	quarters = atoi(argv[1]) / 25;
-	remaining = atoi(argv[1]) % 25;
+	if (argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	} else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	} else
+	{
+		quarters = atoi(argv[1]) / 25;
+		remaining = atoi(argv[1]) % 25;
 
-	dimes = remaining / 10;
-	remaining2 = remaining % 10;
+		dimes = remaining / 10;
+		remaining2 = remaining % 10;
 
-	nickels = remaining2 / 5;
-	remaining3 = remaining2 % 5;
+		nickels = remaining2 / 5;
+		remaining3 = remaining2 % 5;
 
-	cent2 = remaining3 / 2;
-	penny = remaining3 % 2;
+		cent2 = remaining3 / 2;
+		penny = remaining3 % 2;
 
-	final = quarters + dimes + nickels + cent2 + penny;
-
-	printf("%d\n", final);
+		final = quarters + dimes + nickels + cent2 + penny;
+		printf("%d\n", final);
+	}
 }
