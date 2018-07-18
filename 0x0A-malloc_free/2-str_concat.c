7b7f65b9cec48299f14cc94bfac887021e96f238
Heindrick Cheung <hcheung01@gmail.com>
2018-07-18 00:54:46 +0000

    update

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *str_concat - function with two arguments
+ * @s1: char type pointer
+ * @s2: char type pointer
+ *
+ * Description: concatenates two strings
+ * Return: Null for failure or concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int i, i2, j, k;
+	if (s1 == NULL)
+	{
+		s1 = '\0';
+		return (NULL);
+	}
+	if (s2 == NULL)
+	{
+		s2 = '\0';
+		return (NULL);
+	}
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	i2 = 0;
+	while (s2[i2] != '\0')
+		i2++;
+
+	ptr = malloc(sizeof(char) * i + i2 + 1);
+
+	j = 0;
+	while (s1[j] != '\0')
+	{
+		ptr[j] = s1[j];
+		j++;
+	}
+
+	k = 0;
+	while (s2[k] != '\0')
+	{
+		ptr[j] = s2[k];
+		j++;
+		k++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
