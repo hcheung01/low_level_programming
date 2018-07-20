b0f1ff580c8b4a825da44c993199ffa757dff846
Heindrick Cheung <hcheung01@gmail.com>
2018-07-20 02:38:34 +0000

    update

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *_calloc - function with two argument
+ * @nmemb: member of block
+ * @size: size of memory
+ *
+ * Description: allocates memory for an array
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *ptr, i;
+
+	if (nmemb <= 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = 0;
+	return (ptr);
+}
