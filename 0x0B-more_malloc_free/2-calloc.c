93fb572d328fd98d57d56d9d02e2a6e6a6a33ebe
Heindrick Cheung <hcheung01@gmail.com>
2018-07-20 02:42:13 +0000

    update

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *ptr, i;
 
-	if (nmemb <= 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(size * nmemb);
