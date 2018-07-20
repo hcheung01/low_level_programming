fa7c1ba41c51311af1692c177c0c4b1984ad0f51
Heindrick Cheung <hcheung01@gmail.com>
2018-07-20 19:45:11 +0000

    function that allocate memory according to new size

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *_realloc - function with 3 parameter
+ * @ptr: pointer to old memory
+ * @old_size: size of old memory
+ * @new_size: size of new memory
+ *
+ * Description: allocate memory for new size
+ * Return: pointer to new memory allocated
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *np;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		np = malloc(new_size);
+		if (np == NULL)
+		{
+			free(np);
+			return (NULL);
+		}
+		return (np);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	np = malloc(new_size);
+	if (np == NULL)
+	{
+		free(np);
+		return (NULL);
+	}
+
+	for (i = 0; i < old_size; i++)
+		np[i] = *((char *)ptr + i);
+
+	free(ptr);
+	return (np);
+}
