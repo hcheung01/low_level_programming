e93f874a7eac25591b098715b84d4eb7d87df6fc
Heindrick Cheung <hcheung01@gmail.com>
2018-07-24 02:48:43 +0000

    update

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  * Description: store different data types
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
