870ff7c94aea44933da834942a26df24f3744bfd
Heindrick Cheung <hcheung01@gmail.com>
2018-07-24 01:24:37 +0000

    update

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - function with 4 arguments
+ * @d: structure pointer
+ * @name: pointer character
+ * @age: float type argument
+ * @owner: pointer type character
+ *
+ * Description: initialize a variable of type
+ * Return: na
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
