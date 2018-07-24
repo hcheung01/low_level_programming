02692ca8c328d64f0d74bf1959511c0e872ac775
Heindrick Cheung <hcheung01@gmail.com>
2018-07-24 04:39:34 +0000

    update with return

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -22,13 +22,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_d->name = _strdup(name);
 	if (!new_d->name)
+	{
 		free(new_d);
+		return (NULL);
+	}
 	new_d->age = age;
 	new_d->owner = _strdup(owner);
 	if (!new_d->owner)
 	{
 		free(name);
 		free(new_d);
+		return (NULL);
 	}
 
 	return (new_d);
