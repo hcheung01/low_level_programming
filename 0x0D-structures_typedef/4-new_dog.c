1628849c8a5b087af232a69eec79f280fae0928b
Heindrick Cheung <hcheung01@gmail.com>
2018-07-24 04:32:26 +0000

    update

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+char *_strdup(char *str);
 
 /**
  * new_dog - function with 3 arguments
@@ -21,8 +22,49 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_d == NULL)
 		return (NULL);
 
-	new_d->name = name;
-       	new_d->age = age;
-       	new_d->owner = owner;
+	new_d->name = _strdup(name);
+	if (!new_d->name)
+		free(name);
+	new_d->age = age;
+	new_d->owner = _strdup(owner);
+	if (!new_d->owner)
+		free(owner);
+
 	return (new_d);
+
+}
+
+/**
+ * *_strdup - function with one argument
+ * @str: string argument
+ *
+ * Description: returns a pointer to allocated space in memory
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(sizeof(char) * i + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
 }
