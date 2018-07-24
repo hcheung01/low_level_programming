c204756c4e48ecbc5defe941fa8f57726bad389f
Heindrick Cheung <hcheung01@gmail.com>
2018-07-24 02:37:06 +0000

    updated spacing

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)");
-		printf("Age: %f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
