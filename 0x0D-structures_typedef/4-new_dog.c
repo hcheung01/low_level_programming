1f00d6d1b1064e1bc26c04be1a743e4a2916193d
Heindrick Cheung <hcheung01@gmail.com>
2018-07-24 03:08:36 +0000

    updated

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - function with 3 arguments
+ * @name: char type pointer
+ * @age: float type
+ * @owner: char type pointer
+ *
+ * Description: creates a new dog
+ * Return: NULL if fail or pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
+		return (NULL);
+
+	new_d->name = name;
+	new_d->age = age;
+	new_d->owner = owner;
+
+	return (new_d);
+}
--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -18,5 +18,5 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
