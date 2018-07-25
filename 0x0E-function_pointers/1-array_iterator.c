6fbb0804d056cb21e57ebe7a52f11426200110de
Heindrick Cheung <hcheung01@gmail.com>
2018-07-25 22:14:04 +0000

    updated checker

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -14,8 +14,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
+	if (array && action)
 	{
+	for (i = 0; i < size; i++)
 		action(array[i]);
 	}
 }
