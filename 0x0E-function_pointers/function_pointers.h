2791bb8e76c45d586ced27a2be949e6cbdcde675
Heindrick Cheung <hcheung01@gmail.com>
2018-07-25 21:50:09 +0000

    included guard for header file

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,3 +1,8 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
