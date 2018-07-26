3101e1d351a9c8063344ace9f5317e7839132263
Heindrick Cheung <hcheung01@gmail.com>
2018-07-26 05:30:40 +0000

    update

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -18,10 +18,10 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
