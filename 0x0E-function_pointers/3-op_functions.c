6ade04889b4da41dd98f36d7c607bc2ec9359844
Heindrick Cheung <hcheung01@gmail.com>
2018-07-26 05:32:11 +0000

    update

--- 0x0E-function_pointers/3-op_functions.c
+++ 0x0E-function_pointers/3-op_functions.c
@@ -69,10 +69,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-        if (b == 0)
+	if (b == 0)
 	{
 		printf("Error\n");
-                exit(100);
+		exit(100);
 	}
 	return (a % b);
 }
