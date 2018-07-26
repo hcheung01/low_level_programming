cc12c05e43a6a16ae1f1e97062df1e86932220ed
Heindrick Cheung <hcheung01@gmail.com>
2018-07-26 05:17:18 +0000

    update

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -18,6 +18,12 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
+	    argv[2][0] != '/' && argv[2][0] != '%' && argv[2][1] != '\0')
+	{
+		print("Error\n");
+		exit(99);
+	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
 	{
 		printf("Error\n");
