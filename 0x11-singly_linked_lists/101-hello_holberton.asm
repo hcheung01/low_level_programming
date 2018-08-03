d23bb18b74fdb8d8b54a5a1a88226967d37289c8
Heindrick Cheung <hcheung01@gmail.com>
2018-08-03 20:04:07 +0000

    print string with assembly code

---
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section.text
+	global main		;declared for linker (ld)
+
+main:
+	mov	edx, len	;len of the string
+	mov	ecx, msg	;message variable
+	mov 	ebx, 1		;write to standard output
+	mov	eax, 4
+	int	0x80		;call kernel
+
+	mov	eax, 1 		;system call (sys_exit)
+	int	0x80		;call kernel
+
+section .data
+msg db 'Hello, Holberton', 0xa	;message to print
+len equ $ - msg			;length of the string
