42c47c1d8ddfb310226a03cd2db800ab6801e0b7
Heindrick Cheung <hcheung01@gmail.com>
2018-08-07 02:26:06 +0000

    update

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *traverse;
 
+	if (head == NULL);
+	{
+		return;
+	}
+
 	while (*head)
 	{
 		traverse = (*head)->next;
 		free(*head);
 		*head = traverse;
 	}
-	(*head) = NULL;
+	*head == NULL;
 }
