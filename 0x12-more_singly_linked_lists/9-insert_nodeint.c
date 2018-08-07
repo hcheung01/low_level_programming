4f2b9412aa5a40f3a119144953e6b4eb7bc6d9a6
Heindrick Cheung <hcheung01@gmail.com>
2018-08-07 04:24:41 +0000

    update

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,6 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			temp->next = cursor->next;
 			cursor->next = temp;
+			break;
 		}
 		count++;
 		cursor = cursor->next;
