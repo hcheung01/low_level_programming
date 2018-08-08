c42222ef32de87f7f36597377b2d171479e66d0f
Heindrick Cheung <hcheung01@gmail.com>
2018-08-08 02:38:16 +0000

    reverse a linked list with one loop and not allowed to use free or malloc

---
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - function with one argument
+ * @head: pointer to head pointer of linked list
+ *
+ * Description: reverses a linked list
+ * Return: pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *linker;
+
+	prev = (*head);
+	linker = (*head)->next;
+	*head = (*head)->next->next;
+
+	prev->next = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		linker->next = prev;
+		prev = linker;
+		linker = (*head);
+		*head = (*head)->next;
+	}
+	(*head)->next = prev;
+	return (*head);
+}
