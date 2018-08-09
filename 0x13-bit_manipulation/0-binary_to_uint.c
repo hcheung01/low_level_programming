975ff8c37eb1879c6f44c10030edfff441cb6867
Heindrick Cheung <hcheung01@gmail.com>
2018-08-09 23:41:57 +0000

    bit manipulation functions

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int _pow_recursion(int x, int y);
+/**
+ * binary_to_uint - function with one argument
+ * @b: pointer to character
+ *
+ * Description: converts a binary number to an unsigned int
+ * Return: converted number or 0 if NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+	unsigned int power = 0;
+	int count = 0;
+
+	/* Get length of string */
+	while (b[count] != '\0')
+	{
+		if (b[count] != '0' && b[count] != '1')
+			return (0);
+		count++;
+	}
+	count -= 1;
+	while (count >= 0)
+	{
+		if (b[count] == '1')
+			value += _pow_recursion(2, power);
+		power++;
+		count--;
+	}
+	return (value);
+}
+
+
+/**
+ * _pow_recursion - function with two arguments
+ * @x: int type 1st argument
+ * @y: int type 2nd argument
+ *
+ * Description: returns the value of x raised to power of y
+ * Return: value of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
---
+++ 0x13-bit_manipulation/README.md
@@ -0,0 +1,27 @@
+# 0x13. C - Bit manipulation
+---
+## Description
+
+This project in the Low Level Programming series is about:
+* Look for the right source of information without too much help
+* How to manipulate bits and use bitwise operators
+
+## Files
+---
+File|Task
+---|---
+0-binary_to_uint.c
+1-print_binary.c
+2-get_bit.c
+3-set_bit.c
+4-clear_bit.c
+5-flip_bits.c
+
+## Directories
+---
+Directory Name | Description
+---|---
+/holbertonschool-low_level_programming/0x13-bit_manipulation
+
+## Author
+Heindrick Cheung
