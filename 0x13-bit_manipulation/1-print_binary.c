9ac06baa591beab05c548e2361c9818b14aa8d82
Heindrick Cheung <hcheung01@gmail.com>
2018-08-10 02:23:39 +0000

    prints the binary representation of a number

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_binary - function with one argument
+ * @n: decimal value
+ *
+ * Description: prints the binary representation of a number
+ * Return: na
+ */
+void print_binary(unsigned long int n)
+{
+	int count, value;
+	unsigned num = n;
+
+	if (n == 0)
+		_putchar('0');
+	while (num > 0)
+	{
+		num = num >> 1;
+		count++;
+	}
+	count--;
+	while (count >= 0)
+	{
+		value = n >> count;
+		if (value & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+		count--;
+	}
+}
