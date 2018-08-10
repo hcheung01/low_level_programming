ec9854e9af172dcb2510c26e1a8ddbfa2324e414
Heindrick Cheung <hcheung01@gmail.com>
2018-08-10 03:33:30 +0000

    count index

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * get_bit - function with two arguments
+ * @n: value to compare
+ * @index: position to compare
+ *
+ * Description: returns the value of a bit at a given index
+ * Return: 0 or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int count = 0;
+
+	count = n >> index;
+
+	if (count & 1)
+		return (1);
+	else
+		return (0);
+}
