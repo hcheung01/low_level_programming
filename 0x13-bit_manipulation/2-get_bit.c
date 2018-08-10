fe104f407def7b892934536b34cf75aa2b9cb82d
Heindrick Cheung <hcheung01@gmail.com>
2018-08-10 03:49:04 +0000

    updated checker

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -10,13 +10,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int count = 0;
-	if (index == 0)
+	if (index > (sizeof(n) * 8))
 		return (-1);
 
-	count = n >> index;
-
-	if (count & 1)
+	n  = n >> index;
+	if (n & 1)
 		return (1);
 	else
 		return (0);
