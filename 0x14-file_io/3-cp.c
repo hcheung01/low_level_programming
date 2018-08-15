b79541e7ab20f5ee268be6c7e213a0c38ae3dee3
Heindrick Cheung <hcheung01@gmail.com>
2018-08-15 04:32:59 +0000

    copy from one file to another in command line

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include "holberton.h"
+
+/**
+ * main - function with two arguments
+ * @argc: count of arguments
+ * @argv: variadic arguments
+ *
+ * Description: copies the content of a file to another file
+ * Return: 0 for success and exit codes for failure
+ */
+int main(int argc, char *argv[])
+{
+	int to = 0, from = 0, to_count = 0;
+	char buffer[1024];
+	int from_count = 1024;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO,
+			"Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	from = open(argv[1], O_RDONLY);
+	if (from == 1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == 1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while (from_count == 1024)
+	{
+		from_count = read(from, buffer, 1024);
+		if (from_count == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		to_count = write(to, buffer, from_count);
+		if (to_count == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (close(from)  == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		exit(100);
+	}
+	if (close(to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
+		exit(100);
+	}
+
+	return (0);
+}
