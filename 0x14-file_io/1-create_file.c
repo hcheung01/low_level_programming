35008ab56b5ac5270dad6fcdcd4b3e4a3b8e9f45
Heindrick Cheung <hcheung01@gmail.com>
2018-08-15 02:04:48 +0000

    added checker for string content to not be empty

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -21,8 +21,16 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[count] != '\0')
-		count++;
+	if (text_content != NULL)
+	{
+		while (text_content[count] != '\0')
+			count++;
+	}
+	else
+	{
+		close(fd);
+		return (1);
+	}
 
 	output = write(fd, text_content, count);
 	if (output == -1 || output != count)
