c727c963eafa7943eb397da9d0b866bd515b79fa
Heindrick Cheung <hcheung01@gmail.com>
2018-08-15 02:01:01 +0000

    creating a new file descriptor

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * create_file - function with two arguments
+ * @filename: pointer to string name
+ * @text_content: pointer to string contents
+ *
+ * Description: creates a file
+ * Return: 1 on success or -1 failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int count = 0;
+	int fd = 0;
+	int output = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[count] != '\0')
+		count++;
+
+	output = write(fd, text_content, count);
+	if (output == -1 || output != count)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
