b91ad9a20b1b56abf9e5ecb579af9d90d80778d0
Heindrick Cheung <hcheung01@gmail.com>
2018-09-15 22:21:14 +0000

    added if conditions to check for edges case of empty list or index

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (h == NULL)
+		return (NULL);
 
 	new->n = n;
 	new->prev = NULL;
