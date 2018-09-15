477606f07ab07e98007bc03edcc3e33faccb71a4
Heindrick Cheung <hcheung01@gmail.com>
2018-09-15 21:51:11 +0000

    added function descriptions on top

--- 0x16-doubly_linked_lists/4-free_dlistint.c
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - function with one argument
+ * @head: pointer to linked list
+ *
+ * Description: free linked list
+ * Return: void
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *cursor = head;
