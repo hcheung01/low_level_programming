aa0b60d729d31c90223e36091034c173d84ad957
Heindrick Cheung <hcheung01@gmail.com>
2018-09-15 21:22:42 +0000

    updated list header to include stdlib

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function with two arguments
+ * @head: pointer to double linked list
+ * @n: data for each node
+ *
+ * Description: adds a new node at the beginning
+ * Return: address of new element or NULL if failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = *head;
+		*head = new;
+	}
+	return (new);
+}
--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H_
 #define LISTS_H_
-
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * struct dlistint_s - doubly linked list
