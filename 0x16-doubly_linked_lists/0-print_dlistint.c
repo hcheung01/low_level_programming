a7976c6c32e6a200e93e3af3422a0ddbe9a9389f
Heindrick Cheung <hcheung01@gmail.com>
2018-09-15 21:05:16 +0000

    added checker for if list was empty

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -10,10 +10,18 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	while (h)
+	int count = 0;
+
+	if (h == NULL)
+		return (NULL);
+	else
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
 	}
-	return (0);
+	return (count);
 }
