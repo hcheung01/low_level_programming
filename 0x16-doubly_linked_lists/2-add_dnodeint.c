b96cac004a89beef7bb91e29de30f38c701dcda5
Heindrick Cheung <hcheung01@gmail.com>
2018-09-15 21:27:57 +0000

    fixed conditions

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new = NULL;
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
@@ -26,6 +26,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	else
 	{
 		new->next = *head;
+		if (*head)
+			(*head)->prev = new;
 		*head = new;
 	}
 	return (new);
