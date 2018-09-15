eba50adb6c6dcf56a8b4d1b7bfed85d5f9407d1a
Heindrick Cheung <hcheung01@gmail.com>
2018-09-15 21:06:29 +0000

    correct if conditions

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -14,14 +14,11 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (h == NULL)
 		return (NULL);
-	else
+	while (h)
 	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
