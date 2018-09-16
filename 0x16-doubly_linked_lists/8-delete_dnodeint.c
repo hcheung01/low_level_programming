7196fd59e5f73ab056f5e7ca15d9554b28204845
Heindrick Cheung <hcheung01@gmail.com>
2018-09-16 23:49:08 +0000

    refactored

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,30 +10,22 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *cursor = *head;
+	dlistint_t *ptr = *head;
 
-	if (!head)
-		return (-1);
-	if (head)
+	while (head && index && ptr)
 	{
-		while (index && cursor)
-		{
-			cursor = cursor->next;
-			index--;
-		}
-		if (index)
-			return (-1);
-		if (!index && cursor)
-		{
-			if (cursor->next)
-				cursor->next->prev = cursor->prev;
-			if (cursor->prev)
-				cursor->prev->next = cursor->next;
-			else
-				*head = cursor->next;
-			free(cursor);
-			return (1);
-		}
+		ptr = ptr->next;
+		index--;
+	}
+	if (ptr && !index)
+	{
+		if (ptr->next)
+			ptr->next->prev = ptr->prev;
+	        if (!ptr->prev)
+			*head = ptr->next;
+		else
+			ptr->prev->next = ptr->next;
+		free(ptr);
 	}
 	return (-1);
 }
