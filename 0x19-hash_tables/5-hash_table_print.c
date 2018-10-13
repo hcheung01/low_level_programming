344a31174e1f0d5653ef8b90ce1577f867f8af91
Heindrick Cheung <hcheung01@gmail.com>
2018-10-13 02:31:35 +0000

    print hash table

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function
+ * @ht: pointer to hash table structure
+ *
+ * Description: print all element of hash table
+ * Return: na
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *ptr = NULL;
+	unsigned int index = 0;
+
+	printf("{");
+	while (index < ht->size)
+	{
+		ptr = ht->array[index];
+		while (ptr)
+		{
+			printf("'%s': '%s'", ptr->key, ptr->value);
+			ptr = ptr->next;
+		}
+		index++;
+	}
+	printf("}\n");
+}
