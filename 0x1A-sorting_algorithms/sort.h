9a6f7d4139de61a6bdf997cd0967b4a8fc74fe6c
Heindrick Cheung <hcheung01@gmail.com>
2018-10-30 17:42:23 +0000

    remove extra whitespace

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-        const int n;
-        struct listint_s *prev;
-        struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
