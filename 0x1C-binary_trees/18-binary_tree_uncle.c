f45ade462371dfa1396cf2956a07880519d2bb2a
Heindrick Cheung <hcheung01@gmail.com>
2018-12-14 05:56:19 +0000

    fixed check for uncle

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -9,11 +9,12 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent == NULL
+	    || node->parent->parent == NULL)
 		return (NULL);
-
-	return (
-		(node->parent == node->parent->parent->left) ?
-		node->parent->parent->right : node->parent->parent->left
-		);
+	else
+		return (
+			(node->parent == node->parent->parent->left) ?
+			node->parent->parent->right : node->parent->parent->left
+			);
 }
