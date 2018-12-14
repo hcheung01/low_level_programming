2ef077dea865321f87176cd38c961679fa44e984
Heindrick Cheung <hcheung01@gmail.com>
2018-12-14 02:34:38 +0000

    fixed else condition

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -15,9 +15,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	else if (tree->left == NULL && tree->right == NULL)
 		return (count);
-	else
-	{
-		count = (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-		return (count);
-	}
+	count = (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (count);
 }
