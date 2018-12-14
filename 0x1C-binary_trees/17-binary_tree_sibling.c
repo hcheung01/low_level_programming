c4f39e8301b98cea5b69711d4fbadb064768da85
Heindrick Cheung <hcheung01@gmail.com>
2018-12-14 05:31:19 +0000

    deleted extra whitespace

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -9,7 +9,7 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
 	return (
