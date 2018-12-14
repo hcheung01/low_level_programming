62bd2bcf46789ee4fc558d670572de61885a1780
Heindrick Cheung <hcheung01@gmail.com>
2018-12-14 04:26:20 +0000

    deleted extra function

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -7,11 +7,14 @@
  * Description: balance factor of a binary tree
  * Return: 0 if tree is NULL or height
  */
-/*int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	int l_height = 0, r_height = 0;
 
 	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
 	l_height = binary_tree_balance(tree->left);
@@ -21,22 +24,4 @@
 	if (tree->right)
 		r_height++;
 	return (l_height - r_height);
-}*/
-int binary_tree_balance(const binary_tree_t *tree)
-{
-        int l_height = 0, r_height = 0;
-
-        if (tree == NULL)
-                return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-
-        l_height = binary_tree_balance(tree->left);
-        if (tree->left)
-                l_height++;
-        r_height = binary_tree_balance(tree->right);
-        if (tree->right)
-                r_height++;
-        return (l_height - r_height);
 }
