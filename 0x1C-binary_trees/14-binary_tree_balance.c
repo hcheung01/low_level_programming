799c36c59cc304887bb5459087ec508a69125b3d
Heindrick Cheung <hcheung01@gmail.com>
2018-12-14 05:19:11 +0000

    update

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,7 +1,6 @@
 #include "binary_trees.h"
 
-int help1(const binary_tree_t *tree);
-int help2(const binary_tree_t *tree);
+int helper(const binary_tree_t *tree);
 /**
  * binary_tree_balance - balancing function
  * @tree: pointer to root node
@@ -14,7 +13,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (help1(tree->left) - help2(tree->right));
+	return (helper(tree->left) - helper(tree->right));
 }
 
 /**
@@ -24,25 +23,11 @@ int binary_tree_balance(const binary_tree_t *tree)
  * Description: balance factor of a binary tree
  * Return: 0 if tree is NULL or height
  */
-int help1(const binary_tree_t *tree)
+int helper(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	return (help1(tree->left) + 1);
-}
-
-/**
- * help2 - balancing function
- * @tree: pointer to root node
- *
- * Description: right height
- * Return: 0 if tree is NULL or height
- */
-int help2(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return (help2(tree->right) + 1);
+	return (((helper(tree->left) + 1) > (helper(tree->right) + 1)) ?
+		(helper(tree->left) + 1) : (helper(tree->right) + 1));
 }
