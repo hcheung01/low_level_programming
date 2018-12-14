c884d7aafcaa5760dbd0d183190538185c8e20cc
Heindrick Cheung <hcheung01@gmail.com>
2018-12-14 05:08:48 +0000

    split function

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+int help1(const binary_tree_t *tree);
+int help2(const binary_tree_t *tree);
 /**
  * binary_tree_balance - balancing function
  * @tree: pointer to root node
@@ -9,21 +11,44 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int diff;
-	int l_height, r_height;
+	if (tree == NULL)
+		return (0);
+
+	return (help1(tree->left) - help2(tree->right));
+}
+
+/**
+ * help1 - balancing function
+ * @tree: pointer to root node
+ *
+ * Description: balance factor of a binary tree
+ * Return: 0 if tree is NULL or height
+ */
+int help1(const binary_tree_t *tree)
+{
+	int l;
 
 	if (tree == NULL)
 		return (0);
 
-	l_height = 1 + binary_tree_balance(tree->left);
-	if (tree->right == NULL)
-		l_height++;
-	r_height = 1 + binary_tree_balance(tree->right);
-	if (tree->left == NULL)
-		r_height++;
-	diff = l_height - r_height;
+	l = help1(tree->left);
+	return (l + 1);
+}
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (diff);
-	return (diff);
+/**
+ * help2 - balancing function
+ * @tree: pointer to root node
+ *
+ * Description: right height
+ * Return: 0 if tree is NULL or height
+ */
+int help2(const binary_tree_t *tree)
+{
+	int r;
+
+	if (tree == NULL)
+		return (0);
+
+	r = help2(tree->right);
+	return (r + 1);
 }
