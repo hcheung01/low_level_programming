3436cd84e8ffa3f71292f30c3e99f7fd513b5e3b
Heindrick Cheung <hcheung01@gmail.com>
2018-12-14 03:45:32 +0000

    change algorithm

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -9,17 +9,16 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 0, right = 0, diff;
+	int l_height, r_height;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left)
-		left = 1;
-	if (tree->right)
-		right = 1;
-		diff = left - right;
 
-	return (diff +
-		(binary_tree_balance(tree->left) -
-		 binary_tree_balance(tree->right)));
+	l_height = binary_tree_balance(tree->left) + 1;
+	r_height = binary_tree_balance(tree->right) + 1;
+	if (tree->left == NULL)
+		l_height--;
+	if (tree->right == NULL)
+		r_height--;
+	return (l_height - r_height);
 }
