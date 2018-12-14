18862bfe3c5a6e57e736c9ddb7e0ea0dabcc2308
Heindrick Cheung <hcheung01@gmail.com>
2018-12-14 02:29:04 +0000

    betty guideline updated

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-        if (node && node->left == NULL && node->right == NULL)
+	if (node && node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
 }
