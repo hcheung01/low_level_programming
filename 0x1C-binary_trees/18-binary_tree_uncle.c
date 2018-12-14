b93078126cea3f11ba31c40a5c648006bfdc5e64
Heindrick Cheung <hcheung01@gmail.com>
2018-12-14 05:54:07 +0000

    fixed check for uncle

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -9,23 +9,11 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *uncle = NULL, *granny = NULL;
-
 	if (node == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	granny = node->parent->parent;
-	if (granny->left && granny->right)
-	{
-		if (granny->left->right == node)
-			uncle = granny->right;
-		else if (granny->right->left == node)
-			uncle = granny->left;
-	}
-	else
-		return (NULL);
-
-	binary_tree_uncle(node->left);
-	binary_tree_uncle(node->right);
-	return (uncle);
+	return (
+		(node->parent == node->parent->parent->left) ?
+		node->parent->parent->right : node->parent->parent->left
+		);
 }
