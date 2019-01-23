1a0d423fc960dcb87d3ad73db3e043fcd78cadae
Heindrick Cheung <hcheung01@gmail.com>
2019-01-23 23:46:57 +0000

    jump search algorithm

---
+++ 0x1D-search_algorithms/100-jump.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searching algorithm
+ * @array: array of integers, sorted
+ * @size: size of the array
+ * @value: key value to match an element of the array
+ *
+ * Description: Using a jump search algorithm to find matching value
+ * Return: -1 if not found or index to the matched value
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int sq_root = sqrt(size);
+	size_t i, j;
+
+	for (i = 0; i < size; i = i + sq_root)
+	{
+		if (array[i] == value)
+		{
+			break;
+		}
+		if (array[i] > value)
+			break;
+		if (array[i] < value)
+			printf("Value checked array[%li] = [%d]\n", i, array[i]);
+	}
+	printf("Value found between indexes [%li] and [%li]\n", i - sq_root, i);
+	j = i;
+	i -= sq_root;
+	for (; i <= j; i++)
+	{
+		printf("Value checked array[%li] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		if (value > array[j])
+			break;
+	}
+	return (-1);
+}
