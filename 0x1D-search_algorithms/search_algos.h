8d5d5130d6e24066e86a07dfa3ee4a6f4d14e085
Heindrick Cheung <hcheung01@gmail.com>
2019-01-23 23:48:23 +0000

    added jump search prototype to header

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -1,7 +1,9 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
 #include <stdio.h>
+#include <math.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int bs_helper(int *array, int key, int lower, int higher);
+int jump_search(int *array, size_t size, int value);
 #endif
