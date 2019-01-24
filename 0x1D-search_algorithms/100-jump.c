8a5880fe2a9fa7da2d00b15a0137255a43205118
Heindrick Cheung <hcheung01@gmail.com>
2019-01-24 03:04:40 +0000

    change conditions

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -14,18 +14,21 @@ int jump_search(int *array, size_t size, int value)
 	unsigned int step = 0;
 	unsigned int prev = 0;
 
+	if (array == NULL || size == 0)
+		return (-1);
+
 	while (array[step] < value && step < size)
 	{
-		prev = step;
 		printf("Value checked array[%d] = [%d]\n", step, array[step]);
+		prev = step;
 		step += sqrt(size);
+		if (prev > size)
+			break;
 	}
 	printf("Value found between indexes [%d] and [%d]\n", prev, step);
-
 	while (prev < size)
 	{
 		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-
 		if (value == array[prev])
 			return (prev);
 		prev++;
